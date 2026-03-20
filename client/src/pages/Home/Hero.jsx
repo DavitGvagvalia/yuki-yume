3ea2eadedfff37b35296439310afff4b9ab95c1f
DavitGvagvalia <davitgvagvalia17@gmail.com>
2026-03-20 22:57:22 +0400

    firebase rebasing

--- client/src/pages/Home/Hero.jsx
+++ client/src/pages/Home/Hero.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import heroImage from '../../../assets/images/heroImage.png';
+import heroImage from '../../../assets/images/heroImage.webp';
 function Hero() {
     return (
         <section
